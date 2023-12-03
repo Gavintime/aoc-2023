32f2b27da5262a3016969137925b9c8c9521e4a5
Gavintime <16593223+Gavintime@users.noreply.github.com>
2023-12-02 18:58:45 -0800

    add eslint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-console.log("hello aoc 2023!");
+console.log('hello aoc 2023!')
 
-const addNumber = (a:number , b:number) => {
-    return a + b;
-};
+const addNumber = (a: number, b: number): number => {
+  return a + b
+}
 
-console.log(addNumber(5,10));
+console.log(addNumber(5, 10))
